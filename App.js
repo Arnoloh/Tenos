676baa310eb8603d6720d77f4ac2e72fef342b9a
Arnoloh <nono.corcione@gmail.com>
2022-06-17 12:30:23 +0200

    bouton with log

--- App.js
+++ App.js
@@ -1,39 +1,68 @@
-import { SafeAreaView, TextInput, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Button, SafeAreaView, Alert, TextInput, Text, StyleSheet, View } from "react-native";
+import React, { Component } from 'react';
 
 function App() {
 
   return (
-    <NeedText>Send</NeedText>
-  )
-};
-
-const NeedText = () => {
-  const [text, onChangeText] = React.useState(null);
-  return (
     <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder="useless placeholder"
-      />
+      <div>
+        <View style={styles.styleButton}>
+          <NeedText></NeedText>
+        </View>
+      </div>
     </SafeAreaView>
   )
+
+};
+
+class NeedText extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: ''
+    };
+  };
+  message() {
+    const { message } = this.state.message;
+  }
+  render() {
+    return (
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({ message: text })}
+          value={this.state.message}
+          placeholder="Write your message here !"
+        />
+        <Button
+          color={"#047AFF"}
+          title="Send message"
+          onPress={() => console.log(this.state.message)}
+        />
+      </SafeAreaView >
+    )
+  }
 }
 const styles = StyleSheet.create({
-
   MainContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
   },
   input: {
     height: 40,
-    margin: 12,
+    margin: 10,
     borderWidth: 1,
     padding: 10,
-    alignItems:'bottom'
   },
+  bot: {
+    position: "absolute",
+    width: "50%",
+    bottom: "10px",
+    border: "3px solid #8AC007"
+
+  }
+
 });
-export default NeedText;
+
+
+export default App;
